726c30ca8d2cc5082d6e027fa33f424deaadab8e
Venu <venu996366@gmail.com>
2025-04-17 23:38:57 +0530

    Renamed main.js to main.jsx and added _redirects for Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,4 @@
-// // index.js
-// import React from 'react';
-// import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
-// import App from './App';
-// import './index.css';
 
-// // Use createRoot instead of ReactDOM.render
-// const root = createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // <-- Correct import (without curly braces)
